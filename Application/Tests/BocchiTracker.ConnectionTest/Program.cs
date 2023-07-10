c78ab0cc6e5e8e277a7768fe6e7879e19c3b53d1
KirisameMarisa <marisa.love0925@gmail.com>
2023-07-10 23:18:49 +0900

    fix connection test

--- Application/Tests/BocchiTracker.ConnectionTest/Program.cs
+++ Application/Tests/BocchiTracker.ConnectionTest/Program.cs
@@ -11,42 +11,81 @@ using System.Xml.Linq;
 using System;
 using System.Threading.Tasks;
 using DryIoc.Microsoft.DependencyInjection;
+using BocchiTracker.ProcessLink.ProcessData;
+using BocchiTracker.ProcessLinkQuery.Queries;
+using System.Data.Common;
 
 namespace BocchiTracker.ProcessLink.Test
 {
+    public class ProcessDataConnectionTest : IProcessData
+    {
+        public Task Process(IMediator inMediator, int inClientID, Packet inPacket)
+        {
+            if (inPacket.QueryIdType != QueryID.PlayerPosition)
+                return Task.CompletedTask;
+
+            var data = inPacket.QueryIdAsPlayerPosition();
+            var status = new Dictionary<string, string>();
+            status["X"] = data.X.ToString();
+            status["Y"] = data.Y.ToString();
+            status["Z"] = data.Z.ToString();
+            status["Stage"] = data.Stage;
+
+            System.Console.WriteLine($"Position({status["X"]}, {status["Y"]}, {status["Z"]}), Stage({status["Stage"]})");
+            return Task.CompletedTask;
+        }
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
             var services = new ServiceCollection();
 
-            // テスト用のポート番号とMediatorのインスタンスを準備
             int port = 12345;
             var mediator = BuildMediator();
+            var serviceProcessData = BuildServiceProcessData();
 
-            // Connectionインスタンスを生成
-            var connection = new Connection(port, mediator);
+            var connection = new Connection(port, mediator, serviceProcessData);
 
-            // Connectionを非同期で開始
             var connectionTask = connection.StartAsync();
 
-            // Connectionの終了を待機
+            await WiatForExit(connection);
+
             await connectionTask;
+        }
+
+        private static Task WiatForExit(Connection ioConnection)
+        {
+            Console.WriteLine("Press 'q' to stop the connection...");
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.KeyChar == 'q')
+                {
+                    ioConnection.Stop();
+                    Console.WriteLine("Connection stopped.");
+                    break;
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        private static IServiceProcessData BuildServiceProcessData()
+        {
+            var service = new ServiceProcessData();
+            service.Register(QueryID.PlayerPosition, new ProcessDataConnectionTest());
+            return service;
         }
 
         private static IMediator BuildMediator()
         {
             var container = new Container();
-            // Since Mediator has multiple constructors, consider adding rule to allow that
-            // var container = new Container(rules => rules.With(FactoryMethod.ConstructorWithResolvableArguments))
-
+ 
             container.Use<TextWriter>(Console.Out);
 
-            //Pipeline works out of the box here
-
             container.RegisterMany(new[] { typeof(IMediator).GetAssembly(), typeof(Ping).GetAssembly() }, Registrator.Interfaces);
-            //Without the container having FactoryMethod.ConstructorWithResolvableArguments commented above
-            //You must select the desired constructor
             container.Register<IMediator, Mediator>(made: Made.Of(() => new Mediator(Arg.Of<IServiceProvider>())));
 
             var services = new ServiceCollection();
