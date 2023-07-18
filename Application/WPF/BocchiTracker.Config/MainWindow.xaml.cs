c0a06290d15d63173505ec6650dbafb4de84e75a
KirisameMarisa <marisa.love0925@gmail.com>
2023-07-18 16:22:05 +0900

    changed project name of bocchitracker.config to bocchitracker.configapp

--- Application/WPF/BocchiTracker.Config/MainWindow.xaml.cs
+++ Application/WPF/BocchiTracker.Config/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BocchiTracker.Config
+namespace BocchiTracker.ConfigApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
