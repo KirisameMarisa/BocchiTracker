59b6ed6428e702c733171d5c23350f8c76950baa
KirisameMarisa <marisa.love0925@gmail.com>
2023-08-15 13:38:24 +0900

    moved unitydll directory

--- Plugins/Unity/UnityDLL/Setup.bat
+++
@@ -1,7 +0,0 @@
-echo
-set /p UnityDLL="where is unity dll dir? : "
-
-copy /B /Y %UnityDLL%\UnityEditor.dll %~dp0\UnityEditor.dll
-copy /B /Y %UnityDLL%\UnityEngine.dll %~dp0\UnityEngine.dll
-
-pause
