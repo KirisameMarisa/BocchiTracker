3143fad57b356049615bca3bb8123815eac3f5c8
KirisameMarisa <marisa.love0925@gmail.com>
2023-09-14 13:53:04 +0900

    * UploadFilesView can display preview screenshots

--- Application/Models/IssueAssetCollector/AssetData.cs
+++ Application/Models/IssueAssetCollector/AssetData.cs
@@ -1,22 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Jpeg;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using SixLabors.ImageSharp.Formats;
 
 namespace BocchiTracker.IssueAssetCollector
 {
     public class AssetData
     {
+        public List<string> SupportExts { get; private set; } = new List<string> 
+        { 
+            ".png",
+            ".jpg",
+            ".log",
+            ".mp4"
+        };
+
+        public Dictionary<string, ImageEncoder> SupportImages { get; private set; } = new Dictionary<string, ImageEncoder>
+        {
+            { ".png", new PngEncoder()  },
+            { ".jpg", new JpegEncoder() },
+        };
+
         public string Name { get; set; } = string.Empty;
 
         public string FullName { get; set; } = string.Empty;
+
+        public string Extension { get; set; } = string.Empty;
+
+        public byte[]? PictureRawData { get; set; }
 
         public AssetData(string inFilename)
         {
             FullName = inFilename;
             Name = Path.GetFileName(FullName);
+            
+            var ext = Path.GetExtension(FullName);
+            if(SupportExts.Contains(ext))
+            {
+                Extension = ext;
+
+                if(SupportImages.ContainsKey(ext))
+                {
+                    using Image<Bgra32> image = Image.Load<Bgra32>(FullName);
+                    using MemoryStream stream = new MemoryStream();
+                    image.Save(stream, SupportImages[ext]);
+                    PictureRawData = stream.ToArray();
+                }
+            }
         }
     }
 }
--- Application/WPF/BocchiTracker.Client/Views/ReportParts/UploadFilesView.xaml.cs
+++ Application/WPF/BocchiTracker.Client/Views/ReportParts/UploadFilesView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BocchiTracker.Client.ViewModels.ReportParts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,38 @@ namespace BocchiTracker.Client.Views.ReportParts
     /// </summary>
     public partial class UploadFilesView : UserControl
     {
+        private UploadItem _curSelectItem = null;
+        private Grid _curSelectElement = null;
+
         public UploadFilesView()
         {
             InitializeComponent();
+        }
+
+        private void OnItemClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Grid element && element.DataContext is UploadItem selectedItem)
+            {
+                if (_curSelectElement != null)
+                {
+                    var visual = _curSelectElement;
+                    if (visual != null)
+                        visual.Background = element.Background;
+                }
+
+                if(_curSelectItem != null) 
+                {
+                    var item = _curSelectItem;
+                    if (item != null)
+                        item.IsSelected = false;
+                }
+
+                element.Background = Brushes.LightGray;
+                _curSelectElement = element;
+
+                selectedItem.IsSelected = true;
+                _curSelectItem = selectedItem;
+            }
         }
     }
 }
