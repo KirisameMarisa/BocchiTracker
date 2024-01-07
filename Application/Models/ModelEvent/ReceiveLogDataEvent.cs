e47dbc506555f2f4d1bbe8e4889650170de813a4
KirisameMarisa <marisa.love0925@gmail.com>
2024-01-08 01:43:15 +0900

    added the ability logging

---
+++ Application/Models/ModelEvent/ReceiveLogDataEvent.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BocchiTracker.ModelEvent
+{
+    public class ReceiveLogDataEventParameter
+    {
+        public List<string> Log { get; set; }
+
+        public ReceiveLogDataEventParameter(List<string> inLog)
+        {
+            Log = inLog;
+        }
+    }
+
+    public class ReceiveLogDataEvent : PubSubEvent<ReceiveLogDataEventParameter> { }
+}
---
+++ Application/Models/ProcessLink/ProcessData/ProcessDataLogData.cs
@@ -0,0 +1,27 @@
+﻿using BocchiTracker.ModelEvent;
+using BocchiTracker.ProcessLinkQuery.Queries;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BocchiTracker.ProcessLink.ProcessData
+{
+    public class ProcessDataLogData : IProcessData
+    {
+        public void Process(IEventAggregator inEventAggregator, int inClientID, Packet inPacket)
+        {
+            var data = inPacket.QueryIdAsLogData();
+            var logData = new List<string>();
+            
+            for (int i = 0; i < data.LogLength; ++i)
+                logData.Add(data.Log(i));
+
+            inEventAggregator
+                .GetEvent<ReceiveLogDataEvent>()
+                .Publish(new ReceiveLogDataEventParameter(logData));
+        }
+    }
+}
