3ed5fdaa780c43b47e429845a8f8acdeac87317f
AritaYuto <marisa.love0925@gmail.com>
2024-01-21 00:21:52 +0900

    Update build_godot.py

--- ExternalTools/BuildScripts/build_godot.py
+++ ExternalTools/BuildScripts/build_godot.py
@@ -2,7 +2,7 @@ import subprocess
 import config
 
 def Build():
-    subprocess.call(["python", "-m", "SConstruct"], cwd=config.cGodotPath)
+    subprocess.call(["python", "-m", "SCons"], cwd=config.cGodotPath)
 
 if __name__ == '__main__':
     Build()
