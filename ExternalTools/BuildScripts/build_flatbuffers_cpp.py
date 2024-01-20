9b2696925d03f776cff1419b24f3e745f72af113
KirisameMarisa <marisa.love0925@gmail.com>
2024-01-20 22:54:21 +0900

    fix script

--- ExternalTools/BuildScripts/build_flatbuffers_cpp.py
+++ ExternalTools/BuildScripts/build_flatbuffers_cpp.py
@@ -17,14 +17,14 @@ def Build():
     shutil.copytree(include_path, unreal_engine_include_path.resolve(), dirs_exist_ok=True)
 
     if platform.system() == "Windows":
-        release_lib_path = config.cFlatBuffersPath / "Release"
+        release_lib_path = config.cFlatBuffersPath / "Release" / "flatbuffers.lib"
         unreal_engine_lib_path = config.cUnrealEnginePath / "ThirdParty" / "flatbuffers" / "lib" / "Win64" / "Release"
     else:
-        release_lib_path = config.cFlatBuffersPath / "build" / "lib"
+        release_lib_path = config.cFlatBuffersPath / "libflatbuffers,a"
         unreal_engine_lib_path = config.cUnrealEnginePath / "ThirdParty" / "flatbuffers" / "lib" / "Unix" / "Release"
 
-    unreal_engine_lib_path.resolve().mkdir(exist_ok=True)
-    shutil.copytree(release_lib_path.resolve(), unreal_engine_lib_path.resolve(), dirs_exist_ok=True)
+    unreal_engine_lib_path.resolve().mkdir(exist_ok=True, parents=True)
+    shutil.copy(release_lib_path.resolve(), unreal_engine_lib_path.resolve(), dirs_exist_ok=True)
 
 def RunMsBuild():
     subprocess.call(["cmake", "-G", "Visual Studio 17", "-DCMAKE_BUILD_TYPE=Release"], cwd=config.cFlatBuffersPath)
