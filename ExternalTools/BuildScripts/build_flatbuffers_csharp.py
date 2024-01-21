91a3e701b017b2af9f93eaaf9fec2bc9fa89275f
KirisameMarisa <marisa.love0925@gmail.com>
2024-01-21 12:26:44 +0900

    update script

--- ExternalTools/BuildScripts/build_flatbuffers_csharp.py
+++ ExternalTools/BuildScripts/build_flatbuffers_csharp.py
@@ -4,9 +4,6 @@ import shutil
 import config
 
 def Build():
-    print("Cmake C# flatbuffers")
-    subprocess.call(["cmake", "-G", "Visual Studio 17", "-DCMAKE_BUILD_TYPE=Release"], cwd=config.cFlatBuffersPath)
-
     print("Build C# flatbuffers")
     flatbuffers_csproj_path = config.cFlatBuffersPath / "net" / "FlatBuffers" / "Google.FlatBuffers.csproj"
     subprocess.call(["dotnet", "build", flatbuffers_csproj_path.resolve(), "-c", "Release"], cwd=config.cFlatBuffersPath)
