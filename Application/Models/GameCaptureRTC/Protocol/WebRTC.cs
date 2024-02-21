77f8487f698c2b376eac95b69be6d0e4409b96df
KirisameMarisa <marisa.love0925@gmail.com>
2024-02-21 10:02:06 +0900

    supported by PixelStreaming of UE5

--- Application/Models/GameCaptureRTC/Protocol/WebRTC.cs
+++ Application/Models/GameCaptureRTC/Protocol/WebRTC.cs
@@ -10,9 +10,105 @@ using SIPSorcery.Net;
 using SIPSorceryMedia.Abstractions;
 using SIPSorceryMedia.FFmpeg;
 using BocchiTracker.Config.Configs;
+using WebSocketSharp;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
 
 namespace BocchiTracker.GameCaptureRTC.Protocol
 {
+    public class WebRTCWebSocketPeer : WebSocketBehavior
+    {
+        private RTCPeerConnection _pc = default!;
+
+        public Func<Task<RTCPeerConnection>> CreatePeerConnection = default!;
+
+        public RTCPeerConnection RTCPeerConnection => _pc;
+
+        public RTCOfferOptions OfferOptions { get; set; } = default!;
+
+        public RTCAnswerOptions AnswerOptions { get; set; } = default!;
+
+        protected override async void OnMessage(MessageEventArgs e)
+        {
+            RTCSessionDescriptionInit init2;
+            if (RTCIceCandidateInit.TryParse(e.Data, out var init))
+            {
+                Console.WriteLine("Got remote ICE candidate.");
+                _pc.addIceCandidate(init);
+            }
+            else if (RTCSessionDescriptionInit.TryParse(e.Data, out init2))
+            {
+                Console.WriteLine($"Got remote SDP, type {init2.type}.");
+                SetDescriptionResultEnum setDescriptionResultEnum = _pc.setRemoteDescription(init2);
+                if (setDescriptionResultEnum != 0)
+                {
+                    Console.WriteLine($"Failed to set remote description, {setDescriptionResultEnum}.");
+                    _pc.Close("failed to set remote description");
+                    base.Close();
+                }
+                else if (_pc.signalingState == RTCSignalingState.have_remote_offer)
+                {
+                    RTCSessionDescriptionInit answerSdp = _pc.createAnswer(AnswerOptions);
+                    await _pc.setLocalDescription(answerSdp).ConfigureAwait(continueOnCapturedContext: false);
+                    Console.WriteLine($"Sending SDP answer to client {Context.UserEndPoint}.");
+                    Context.WebSocket.Send(AddPlayerIdJson(answerSdp.toJSON()));
+                }
+            }
+            else
+            {
+                Console.WriteLine("websocket-server could not parse JSON message. " + e.Data);
+            }
+        }
+
+        protected override async void OnOpen()
+        {
+            base.OnOpen();
+            Console.WriteLine($"Web socket client connection from {Context.UserEndPoint}.");
+            _pc = await CreatePeerConnection().ConfigureAwait(continueOnCapturedContext: false);
+
+            var rtc_config = _pc.getConfiguration();
+            var data = Newtonsoft.Json.JsonConvert.SerializeObject(
+                new Dictionary<string, object>
+                {
+                    { "type",                   "config" },
+                    { "peerConnectionOptions", new Dictionary<string, object> {
+                            { "bundlePolicy",           rtc_config.bundlePolicy.ToString() },
+                            { "certificates",           rtc_config.certificates },
+                            { "iceCandidatePoolSize",   rtc_config.iceCandidatePoolSize },
+                            { "iceServers",             rtc_config.iceServers },
+                            { "iceTransportPolicy",     rtc_config.iceTransportPolicy.ToString() },
+                            { "rtcpMuxPolicy",          rtc_config.rtcpMuxPolicy.ToString() },
+                        }
+                    },
+                }
+            );
+            Context.WebSocket.Send(data);
+
+            _pc.onicecandidate += delegate (RTCIceCandidate iceCandidate)
+            {
+                if (_pc.signalingState == RTCSignalingState.have_remote_offer || _pc.signalingState == RTCSignalingState.stable)
+                {
+                    Context.WebSocket.Send(AddPlayerIdJson(iceCandidate.toJSON()));
+                }
+            };
+
+            if (Context.QueryString["role"] != "offer")
+            {
+                RTCSessionDescriptionInit offerSdp = _pc.createOffer(OfferOptions);
+                await _pc.setLocalDescription(offerSdp).ConfigureAwait(continueOnCapturedContext: false);
+                Console.WriteLine($"Sending SDP offer to client {Context.UserEndPoint}.");
+                Context.WebSocket.Send(AddPlayerIdJson(offerSdp.toJSON()));
+            }
+        }
+
+        private string AddPlayerIdJson(string inJson)
+        {
+            string custom_json = inJson;
+            custom_json = custom_json.Insert(inJson.LastIndexOf('}'), ", \"playerid\": \"BocchiTrackerPlayer\"");
+            return custom_json;
+        }
+    }
+
     public class WebRTC : WebSocketBehavior, ICaptureProtocol, IDisposable
     {
         private readonly IEventAggregator _eventAggregator;
@@ -25,6 +121,8 @@ namespace BocchiTracker.GameCaptureRTC.Protocol
             _eventAggregator = inEventAggregator;
             _captureFrameStorage = new CaptureFrameStorage(inUserCaptureSetting.RecordingFrameRate, inUserCaptureSetting.RecordingMintes);
             _web_socket = new WebSocketServer(IPAddress.Any, inPort, inSecure);
+            _web_socket.Log.Level = WebSocketSharp.LogLevel.Trace;
+            _web_socket.AllowForwardedRequest = true;
             _web_socket.AddWebSocketService<WebRTCWebSocketPeer>("/", (peer) => peer.CreatePeerConnection = () => CreatePeerConnection(inCaptureSetting, inUserCaptureSetting, _captureFrameStorage));
             _web_socket.Start();
         }
