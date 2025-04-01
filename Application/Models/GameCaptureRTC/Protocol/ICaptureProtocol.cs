f87ddcf30dc8dbf7d1155e5fb10d0df5e486b971
AritaYuto <yuto.arita@aktsk.jp>
2025-04-01 18:16:08 +0900

    Blazorを足して、ビルドエラー解消

--- Application/Models/GameCaptureRTC/Protocol/ICaptureProtocol.cs
+++ Application/Models/GameCaptureRTC/Protocol/ICaptureProtocol.cs
@@ -1,11 +1,4 @@
-﻿using BocchiTracker.Config.Configs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BocchiTracker.GameCaptureRTC.Protocol
+﻿namespace BocchiTracker.GameCaptureRTC.Protocol
 {
     public interface ICaptureProtocol
     {
